b735cdbca2ff440470ca5599947f651ce28c71f3
suzii19 <60194135004@senacrs.edu.br>
2025-09-08 21:46:26 -0300

    atualizando

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -1,7 +1,17 @@
-function desenhaQuad(x,y,cor){
-    con.fillStyle = cor;
-    con.fillRect(x*sq,y*sq,sq,sq);
+const cvs = document.getElementById("tetris");// pega o elemento canvas
+const con = cvs.getContext("2d");// pega o contexto 2d do canvas
+const scoreElement = document.getElementById("score");// pega o elemento score do html
 
-    con.strokeStyle = "BLACK";
+const linha = 20;// numero de linhas
+const col = 10;// numero de colunas
+const sq = 20; // tamanho do quadrado
+const vacant = "#FF6E30"// cor do quadro vazio
+
+// desenha o quadro
+function desenhaQuad(x,y,cor){// função que desenha o quadrado
+    con.fillStyle = cor;// 
+    con.fillRect(x*sq,y*sq,sq,sq);// desenha o retangulo
+
+    con.strokeStyle = "#FFC154";// cor da borda
     con.strokeRect(x*sq,y*sq,sq,sq);
 }
