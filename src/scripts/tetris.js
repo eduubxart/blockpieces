d372d35b7936fe53c511b6fa10cbb18b66f75b88
SUZANA COSTA PAULO <60210878010@senacrs.edu.br>
2025-09-15 20:50:20 -0300

    primeiro commit

--- src/scripts/tetris.js
+++ src/scripts/tetris.js
@@ -1,29 +1,238 @@
-const cvs = document.getElementById("tetris");// pega o elemento canvas
-/*cvs.getContext("2d") --> ele ta permitindo que desenhe em retangulos,linhas e etc... no canvas*/
-const con = cvs.getContext("2d");// pega o contexto 2d do canvas
-const scoreElement = document.getElementById("score");// pega o elemento score do html
+const cvs = document.getElementById("tetris");
+const con = cvs.getContext("2d");
+const scoreSist = document.getElementById("score");
 
-const linha = 20;// numero de linhas
-const col = 10;// numero de colunas
-const sq = 20; // tamanho do quadrado
-const vacant = "#FF6E30"// cor do quadro vazio
+const linha = 20;
+const col = 10;
+const sq = 20;
+const quad = "WHITE";
 
-cvs.width = col * sq;      // largura do canvas = número de colunas * tamanho de cada quadradinho
-cvs.height = linha * sq;   // altura do canvas = número de linhas * tamanho de cada quadradinho
-con.imageSmoothingEnabled = false; // desliga o "suavizador de imagens"
-// desenha o quadro
-function desenhaQuad(x, y,asset) {/* desenhaQuad(x,y,cor) função que desenha um quadro da grade*/
-	const px = x * sq; // posição real em px na horizontal
-	const py = y * sq; // posição real em px na vertical
+// Desenhar os quadrados
+function desenhaQuad(x,y,cor){
+    con.fillStyle = cor;
+    con.fillRect(x*sq,y*sq,sq,sq);
 
-	con.fillStyle = cor; //quadrado com a cor escolhida
-	con.fillReact(px, py, sq, sq); // desenha o quadrado
-
-	con.stroleStyle = "black"; // cor da borda do quadrado
-	con.strokeRect(px, py, sq, sq); // desenha a borda do quadrado
+    con.strokeStyle = "BLACK";
+    con.strokeRect(x*sq,y*sq,sq,sq);
 }
-desenhaQuad(3, 5, "blue");// desenha um quadrado azul na posição (3,5)
-desenhaQuad(4,5, vacant);// desenha um quadrado vazio na posição (4,5)
 
-let O_block = new Image();// cria um novo objeto de imagem
-O_block.src = './assets/images/O.png';// define o src da imagem
+// Cria o tabuleiro
+let bord = [];
+for( r = 0; r <linha; r++){
+    bord[r] = [];
+    for(c = 0; c < col; c++){
+        bord[r][c] = quad;
+    }
+}
+// Desenha o tabuleiro
+function tab(){
+    for( r = 0; r <linha; r++){
+        for(c = 0; c < col; c++){
+            desenhaQuad(c,r,bord[r][c]);
+        }
+    }
+}
+tab();
+
+const pecas = [
+    [Z,"red"],
+    [S,"green"],
+    [T,"yellow"],
+    [O,"blue"],
+    [L,"purple"],
+    [I,"cyan"],
+    [J,"orange"]
+];
+
+// gerar peças aleatórias
+function geraPecas(){
+    let r = randomN = Math.floor(Math.random() * pecas.length) // 0 -> 6
+    return new Piece( pecas[r][0],pecas[r][1]);
+}
+
+let p = geraPecas();
+
+function Piece(tetromino,cor){
+    this.tetromino = tetromino;
+    this.cor = cor;
+    
+    this.tetrominoN = 0; // Começa do primeiro padrão
+    this.ativarTetromino = this.tetromino[this.tetrominoN];
+    
+    this.x = 3;
+    this.y = -2;
+}
+
+// função de preenchimento
+Piece.prototype.fill = function(cor){
+    for( r = 0; r < this.ativarTetromino.length; r++){
+        for(c = 0; c < this.ativarTetromino.length; c++){
+            if( this.ativarTetromino[r][c]){
+                desenhaQuad(this.x + c,this.y + r, cor);
+            }
+        }
+    }
+}
+
+// Desenha
+Piece.prototype.draw = function(){
+    this.fill(this.cor);
+}
+
+// Apaga
+Piece.prototype.unDraw = function(){
+    this.fill(quad);
+}
+
+// Move para baixo
+Piece.prototype.moveDown = function(){
+    if(!this.colisao(0,1,this.ativarTetromino)){
+        this.unDraw();
+        this.y++;
+        this.draw();
+    }else{
+        // Trava e gera uma nova
+        this.lock();
+        p = geraPecas();
+    }
+    
+}
+// Mover para a direita
+Piece.prototype.moveRight = function(){
+    if(!this.colisao(1,0,this.ativarTetromino)){
+        this.unDraw();
+        this.x++;
+        this.draw();
+    }
+}
+// Mover para a esquerda
+Piece.prototype.moveLeft = function(){
+    if(!this.colisao(-1,0,this.ativarTetromino)){
+        this.unDraw();
+        this.x--;
+        this.draw();
+    }
+}
+// Gira
+Piece.prototype.rotate = function(){
+    let novoPad = this.tetromino[(this.tetrominoN + 1)%this.tetromino.length];
+    let kick = 0;
+    
+    if(this.colisao(0,0,novoPad)){
+        if(this.x > col/2){
+            kick = -1; 
+        }else{
+            kick = 1; 
+        }
+    }
+    
+    if(!this.colisao(kick,0,novoPad)){
+        this.unDraw();
+        this.x += kick;
+        this.tetrominoN = (this.tetrominoN + 1)%this.tetromino.length; // (0+1)%4 => 1
+        this.ativarTetromino = this.tetromino[this.tetrominoN];
+        this.draw();
+    }
+}
+
+let score = 0;
+
+Piece.prototype.lock = function(){
+    for( r = 0; r < this.ativarTetromino.length; r++){
+        for(c = 0; c < this.ativarTetromino.length; c++){
+            if( !this.ativarTetromino[r][c]){
+                continue;
+            }
+            if(this.y + r < 0){
+                alert("Fim");
+                gameOver = true;
+                break;
+            }
+            bord[this.y+r][this.x+c] = this.cor;
+        }
+    }
+    // Remove linhas inteiras
+    for(r = 0; r < linha; r++){
+        let islinhaFull = true;
+        for( c = 0; c < col; c++){
+            islinhaFull = islinhaFull && (bord[r][c] != quad);
+        }
+        if(islinhaFull){
+            for( y = r; y > 1; y--){
+                for( c = 0; c < col; c++){
+                    bord[y][c] = bord[y-1][c];
+                }
+            }
+            for( c = 0; c < col; c++){
+                bord[0][c] = quad;
+            }
+            // Adiciona +10 aos pontos
+            score += 10;
+        }
+    }
+    // Atualiza o tab
+    tab();
+    
+    // Atualiza a pontuação
+    scoreSist.innerHTML = score;
+}
+// Função de colisão
+
+Piece.prototype.colisao = function(x,y,piece){
+    for( r = 0; r < piece.length; r++){
+        for(c = 0; c < piece.length; c++){
+            // Caso esteja vazio, pula
+            if(!piece[r][c]){
+                continue;
+            }
+            let newX = this.x + c + x;
+            let newY = this.y + r + y;
+            
+            if(newX < 0 || newX >= col || newY >= linha){
+                return true;
+            }
+            if(newY < 0){
+                continue;
+            }
+            if( bord[newY][newX] != quad){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+// Controle
+
+document.addEventListener("keydown",CONTROL);
+
+function CONTROL(event){
+    if(event.keyCode == 37){
+        p.moveLeft();
+        dropStart = Date.now();
+    }else if(event.keyCode == 38){
+        p.rotate();
+        dropStart = Date.now();
+    }else if(event.keyCode == 39){
+        p.moveRight();
+        dropStart = Date.now();
+    }else if(event.keyCode == 40){
+        p.moveDown();
+    }
+}
+// Dropar as peças a cada 1 segundo.
+
+let dropStart = Date.now();
+let gameOver = false;
+function drop(){
+    let now = Date.now();
+    let delta = now - dropStart;
+    if(delta > 1000){
+        p.moveDown();
+        dropStart = Date.now();
+    }
+    if( !gameOver){
+        requestAnimationFrame(drop);
+    }
+}
+
+drop();
