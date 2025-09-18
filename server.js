a20c816848ca0e66d0ac27e51ad518a68c041217
eduubxart <fariabragaeduardo@gmail.com>
2025-09-18 17:11:03 -0300

    atualizando

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const app = express();
+// aqui vamos conectar as rotas
+import express from 'express';//importa o express que é o framework para criar o servidor
+import mongoose from 'mongoose';//importa o mongoose que é o framework para conectar com o banco de dados
+import userRoutes from './routes/userRoutes.js'; //importa as rotas do usuário que foram criadas no arquivo userRoutes.js'
 
-const PORT = 3000;
+const app = express();// cria o servidor usando o express
+app.use(express.json());//configura o servidor para receber dados em formato json
 
-app.get('/', (req, res) => {
-  res.send('Olá! Servidor rodando certinho.');
-});
+//conecta com o mongoDB(banco de dados)
+mongoose.connect('mongodb://localhost:27017/blockpieces', {//aqui ele está conectando com o banco de dados que está rodando na porta 27017 e o nome do banco de dados é blockpieces
+    useNewUrlParser: true,//configuração para usar o novo parser de url
+    useUnifiedTopology: true,//configuração para usar a topologia unificada
+}).then(() => console.log('MongoDB conectado'))//se conectar com sucesso, ele vai mostrar essa mensagem no console
+  .catch(err => console.log(err));//se der algum erro, ele vai mostrar o erro no console
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+  //aqui ele vai usar as rotas do usuário que foram criadas no arquivo userRoutes.js
+app.use('/api/users', userRoutes);//todas as rotas que começarem com /api/users vão ser direcionadas para o arquivo userRoutes.js
+
+app.listen(3000, () => console.log('Servidor rodando na porta 3000'));//aqui ele está configurando o servidor para rodar na porta 3000 e mostrar essa mensagem no console
+
---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';//importa o mongoose que é o framework de banco de dados
+
+const userShema = new mongoose.Schema({//cria o schema do usuário, shema é o modelo do banco de dados
+    username: { type: String, required: true, unique: true },//nome de usuário, string, obrigatório e único
+    password: { type: String, required: true },//senha, string e obrigatório
+    email: { type: String, required: true, unique: true },//email, string, obrigatório e único
+    createdAt: { type: Date, default: Date.now },//data de criação, data e padrão é a data atual
+});
+
+export default mongoose.model('User', userShema);//exporta o modelo do usuário para ser usado em outros arquivos
+//resumido bem, nesta pasta o que eu fiz foi definir como vai ser o modelo do usuário no banco de dados, quais campos ele vai ter e como eles vão ser, e exportei para poder usar em outros arquivos, como no controller e nas rotas.
