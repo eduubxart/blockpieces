d59b1b10e3d59fb44fb46ac577cf342174aaa551
eduubxart <fariabragaeduardo@gmail.com>
2025-09-18 18:00:59 -0300

    ATUALIZANDO

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 // aqui vamos conectar as rotas
 import express from 'express';//importa o express que é o framework para criar o servidor
 import mongoose from 'mongoose';//importa o mongoose que é o framework para conectar com o banco de dados
-import userRoutes from './routes/userRoutes.js'; //importa as rotas do usuário que foram criadas no arquivo userRoutes.js'
+import userRoutes from './server/routes/userRoutes.js'; //importa as rotas do usuário que foram criadas no arquivo userRoutes.js'
 
 const app = express();// cria o servidor usando o express
 app.use(express.json());//configura o servidor para receber dados em formato json
 
 //conecta com o mongoDB(banco de dados)
-mongoose.connect('mongodb://localhost:27017/blockpieces', {//aqui ele está conectando com o banco de dados que está rodando na porta 27017 e o nome do banco de dados é blockpieces
-    useNewUrlParser: true,//configuração para usar o novo parser de url
-    useUnifiedTopology: true,//configuração para usar a topologia unificada
-}).then(() => console.log('MongoDB conectado'))//se conectar com sucesso, ele vai mostrar essa mensagem no console
-  .catch(err => console.log(err));//se der algum erro, ele vai mostrar o erro no console
+mongoose.connect('mongodb+srv://fariabragaeduardo_db_user:senacprojeto09@cluster0.v9vajog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => console.log('MongoDB conectado'))
+    .catch(err => console.log(err));
 
   //aqui ele vai usar as rotas do usuário que foram criadas no arquivo userRoutes.js
 app.use('/api/users', userRoutes);//todas as rotas que começarem com /api/users vão ser direcionadas para o arquivo userRoutes.js
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,10 @@
 // aqui vamos importar o express para criar as rotas
 import express from 'express';//aqui ele vai importar o express
-import { registrerUser } from '../controllers/userController.js'; // importa a função que vai registrar o usuário
+import { registerUser } from '../controllers/userController.js'; // importa a função que vai registrar o usuário
 
 const router = express.Router();// aqui ele vai criar uma rota usando o express
 
-router.post('/register', registrerUser);// aqui ele vai criar uma rota do tipo post para registrar o usuário, quando o frontend fizer uma requisição para essa rota, ele vai chamar a função registrerUser que foi importada do controller
+router.post('/register', registerUser);// aqui ele vai criar uma rota do tipo post para registrar o usuário, quando o frontend fizer uma requisição para essa rota, ele vai chamar a função registrerUser que foi importada do controller
 
 export default router;// aqui ele vai exportar a rota para ser usada em outros arquivos
 //resumido bem, esse arquivo é responsável por criar as rotas da aplicação, nesse caso, a rota para registrar o usuário, e quando essa rota for chamada, ela vai chamar a função que está no controller para registrar o usuário no banco de dados.
