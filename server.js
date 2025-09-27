bf3bd48d8936365a4c440d285585facb553661b6
eduubxart <fariabragaeduardo@gmail.com>
2025-09-27 18:22:23 -0300

    atualizando

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 import userRoutes from './server/routes/userRoutes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,9 +14,9 @@ const app = express();
 app.use(express.json());
 
 // Conectar MongoDB
-mongoose.connect('mongodb+srv://fariabragaeduardo_db_user:sLTwu3KvBix3YNj9@cluster0.v9vajog.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-    .then(() => console.log('MongoDB conectado'))
-    .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB conectado'))
+  .catch(err => console.log(err));
 
 // Rotas do backend (API)
 app.use('/api/users', userRoutes);
@@ -21,9 +24,9 @@ app.use('/api/users', userRoutes);
 // Servir arquivos estáticos do build do Vite
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Rota fallback → só para páginas do frontend, ignora /api
+// Rota fallback → qualquer rota que não seja /api vai pro game
 app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/pages/ranking.html'));
+  res.sendFile(path.join(__dirname, 'dist/pages/game.html')); // abre direto o game
 });
 
 // Iniciar servidor
